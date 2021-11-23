9d3b30e44409579d20c3f0bbb81c81690c5f2fcd
Rohit <rohit.george@atree.org>
2021-11-23 16:54:10 +0530

    eslint ignore rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,9 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'max-len': 0,
+    "semi": 0,
+    "indent": ["error", 4]
   }
 }
